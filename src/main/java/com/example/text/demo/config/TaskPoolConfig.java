0200589657c897b2095f54919ccd2915165ea1b9
1047984367@qq.com <as1314520>
2019-01-10 16:43:09 +0800

    线程池配置

--- src/main/java/com/example/text/demo/config/TaskPoolConfig.java
+++ src/main/java/com/example/text/demo/config/TaskPoolConfig.java
@@ -42,4 +42,22 @@ class TaskPoolConfig {
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return executor;
     }
+    /**
+     * Future接口的五个方法
+     *
+     * cancel方法用来取消任务，如果取消任务成功则返回true，如果取消任务失败则返回false。
+     * 参数mayInterruptIfRunning表示是否允许取消正在执行却没有执行完毕的任务，如果设置true，
+     * 则表示可以取消正在执行过程中的任务。如果任务已经完成，则无论mayInterruptIfRunning为true还是false，
+     * 此方法肯定返回false，即如果取消已经完成的任务会返回false；如果任务正在执行，若mayInterruptIfRunning设置为true，
+     * 则返回true，若mayInterruptIfRunning设置为false，则返回false；
+     * 如果任务还没有执行，则无论mayInterruptIfRunning为true还是false，肯定返回true。
+     *
+     * isCancelled方法表示任务是否被取消成功，如果在任务正常完成前被取消成功，则返回 true。
+     *
+     * isDone方法表示任务是否已经完成，若任务完成，则返回true；
+     *
+     * get()方法用来获取执行结果，这个方法会产生阻塞，会一直等到任务执行完毕才返回；
+     *
+     * get(long timeout, TimeUnit unit)用来获取执行结果，如果在指定时间内，还没获取到结果，就直接返回null。
+     */
 }
