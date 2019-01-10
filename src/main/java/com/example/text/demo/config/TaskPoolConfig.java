288f5fab8f4b2fb3f65db11db17c317c7f335a6a
1047984367@qq.com <as1314520>
2019-01-10 16:47:57 +0800

    Future类注释

--- src/main/java/com/example/text/demo/config/TaskPoolConfig.java
+++ src/main/java/com/example/text/demo/config/TaskPoolConfig.java
@@ -46,8 +46,8 @@ class TaskPoolConfig {
      * Future接口的五个方法
      *
      * cancel方法用来取消任务，如果取消任务成功则返回true，如果取消任务失败则返回false。
-     * 参数mayInterruptIfRunning表示是否允许取消正在执行却没有执行完毕的任务，如果设置true，
-     * 则表示可以取消正在执行过程中的任务。如果任务已经完成，则无论mayInterruptIfRunning为true还是false，
+     * 参数mayInterruptIfRunning表示是否允许取消正在执行却没有执行完毕的任务，如果设置true，则表示可以取消正在执行过程中的任务。
+     * 如果任务已经完成，则无论mayInterruptIfRunning为true还是false，
      * 此方法肯定返回false，即如果取消已经完成的任务会返回false；如果任务正在执行，若mayInterruptIfRunning设置为true，
      * 则返回true，若mayInterruptIfRunning设置为false，则返回false；
      * 如果任务还没有执行，则无论mayInterruptIfRunning为true还是false，肯定返回true。
