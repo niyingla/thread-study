90bc85f7e506dcde45da8622f243a97b77972733
微笑只是问候 <1047984367@qq.com>
2020-06-16 00:00:17 +0800

    增加枚举

---
+++ src/main/java/com/pikaqiu/My/Pizza.java
@@ -0,0 +1,87 @@
+package com.pikaqiu.My;
+
+import lombok.Data;
+
+/**
+ * @program: concurrency
+ * @description: 枚举示例
+ * @author: xiaoye
+ * @create: 2020-06-15 23:05
+ **/
+@Data
+public class Pizza {
+
+    private PizzaStatus status;
+
+    public enum PizzaStatus {
+        //构造方法入参
+        ORDERED(5) {
+            /**
+             * 复写方法
+             * @return
+             */
+            @Override
+            public boolean isOrdered() {
+                return true;
+            }
+        },
+        READY(2) {
+            @Override
+            public boolean isReady() {
+                return true;
+            }
+        },
+        DELIVERED(0) {
+            @Override
+            public boolean isDelivered() {
+                return true;
+            }
+        };
+
+        private int timeToDelivery;
+
+        public boolean isOrdered() {
+            return false;
+        }
+
+        public boolean isReady() {
+            return false;
+        }
+
+        public boolean isDelivered() {
+            return false;
+        }
+
+        public int getTimeToDelivery() {
+            return timeToDelivery;
+        }
+
+        /**
+         * 构造方法
+         * @param timeToDelivery
+         */
+        PizzaStatus(int timeToDelivery) {
+            this.timeToDelivery = timeToDelivery;
+        }
+    }
+
+    public boolean isDeliverable() {
+        return this.status.isReady();
+    }
+
+    public boolean isOrdered() {
+        return this.status.isOrdered();
+    }
+
+    public void printTimeToDeliver() {
+        System.out.println("Time to delivery is " +
+                this.getStatus().getTimeToDelivery());
+    }
+
+    public static void main(String[] args) {
+        Pizza testPz = new Pizza();
+        testPz.setStatus(PizzaStatus.ORDERED);
+        System.out.println(true == testPz.isOrdered());
+    }
+
+}
