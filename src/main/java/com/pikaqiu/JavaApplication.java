ff9eaec444a306c9b9b732c8ffa5d3e0bbab8c24
火火 <1047984367@qq.com>
2021-05-09 16:15:19 +0800

    啦啦啦aa1

--- src/main/java/com/pikaqiu/JavaApplication.java
+++ src/main/java/com/pikaqiu/JavaApplication.java
@@ -1,5 +1,6 @@
 package com.pikaqiu;
 
+import com.pikaqiu.My.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,7 @@ public class JavaApplication {
     public static void main(String[] args) {
         System.out.println("开始启动啦。。。");
         SpringApplication.run(JavaApplication.class, args);
+
+
     }
 }
